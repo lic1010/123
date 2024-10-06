538212825c9cccd784c3fe5ae040b58885994053
your name <your email>
2024-10-06 14:58:55 +0800

    Problem 2.cpp

--- problem_2.cpp
+++ problem_2.cpp
@@ -1,20 +1,31 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-void fum(int a[],int size) {
-    int size_fun=pow(2,size);;
-    for(int i=0;i<size_fun;i++) {
+void  two(int x,int a[]) {// 十轉二
+    int i=0;
+    while(x!=0) {
+        a[i]=x%2;
+        x=x/2;
+        i++;
+    }
+}
+void fun(int size_fun,int b[]) {
+    int a[4];
+    for(int i=0;i<pow(2,size_fun);i++) {
+        two(i,a);
         for(int j=0;j<size_fun;j++) {
-            if() {
-
+            if(a[j]==1) {
+                cout<<b[j];
             }
         }
+        cout<<'\n';
     }
-    cout<<'\n';
 }
 
 int main() {
-    int a[3]={1,2,3};
-    fum(a,3);
+    int b[3]={1,2,3};
+    fun(3,b);
+
+
     return 0;
 }
