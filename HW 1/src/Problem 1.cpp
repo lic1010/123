64299b182cf8ca17f13691cc91d2a5327a0977ca
lic1010 <lee0979338@gmail.com>
2024-10-10 14:36:00 +0800

    Add files via upload

---
+++ HW 1/src/Problem 1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+int f(int m,int n) {
+    if(m==0) return n+1;
+    if(n==0) return f(m-1,1);
+    return f(m-1,f(m,n-1));
+}
+
+int ack(int m, int n) {
+        if(m==0) {
+            return n+1;
+        }
+        else if(m==1) {
+            return n+2;
+        }
+        else if(m==2) {
+            return 2*(n+3)-3;
+        }
+        else if(m==3) {
+            return pow(2,n+3)-3;
+        }
+        else {
+            return -1;
+        }
+    }
+int main() {
+    int x=ack(3,0);
+    cout<< f(3,0)<<endl;
+    if(x==-1)
+        cout<< "輸出數值過大，請重試";
+    else {
+        cout<<x;
+    }
+    return 0;
+}
